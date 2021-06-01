9016aefff632f15aaa54f0ac265215729d7363db
Arun <arun810balaji1@gmail.com>
2021-06-02 01:34:51 +0530

    First Commit

---
+++ src/main/java/com/arun/bootproject1/Bootproject1Application.java
@@ -0,0 +1,13 @@
+package com.arun.bootproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bootproject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bootproject1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/arun/bootproject1/Bootproject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arun.bootproject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bootproject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
